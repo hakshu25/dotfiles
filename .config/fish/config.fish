91b76b77ba5f32ffa23bec4e89418121813364b6
hakshu <flyhighup.air25@gmail.com>
2020-09-12 06:23:32 +0900

    refactor fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,15 @@ set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
 set -g theme_display_git yes
 set -g theme_use_abbreviated_branch_name yes
 set -g theme_display_git_master_branch yes
+# set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
+# set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
+
+# init
+direnv hook fish | source
+starship init fish | source
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
 
 # anyenv
-
 eval (nodenv init - | source)
 set PYENV_ROOT $HOME/.pyenv
 if test -d $PYENV_ROOT
@@ -18,25 +24,7 @@ set GOPATH (go env GOPATH)
 set PATH $PATH:$GOPATH/bin
 set DENO_DIR $HOME/deno
 
-# peco & ghq
-# ghq + peco
-function ghq_peco_repo
-  set selected_repository (ghq list -p | peco --query "$LBUFFER")
-  if [ -n "$selected_repository" ]
-    cd $selected_repository
-    echo " $selected_repository "
-    commandline -f repaint
-  end
-end
-
-function fish_user_key_bindings
-  bind \c] ghq_peco_repo
-end
-
-bind \cr reverse_history_search
-
 # alias
-
 alias playground="cd ~/Desktop/playground"
 alias ls='ls -G'
 alias la='ls -la'
@@ -53,9 +41,3 @@ alias gb='git branch'
 alias gsw='git switch'
 alias gr='git restore'
 alias git-ghq='GHQ_ROOT=~/GitHub ghq'
-direnv hook fish | source
-# set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
-# set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
-starship init fish | source
-
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,6 @@
 function fish_user_key_bindings
   fzf_key_bindings
+  # by hakshu
+  bind \c] ghq_peco_repo
+  bind \cr peco_select_history
 end
