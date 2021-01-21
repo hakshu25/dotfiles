aad2a8a3ef06afb1e276ad38b24fea8997075ce0
hakshu <flyhighup.air25@gmail.com>
2021-01-22 06:26:44 +0900

    Add fish funciton for git checkout by peco

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,5 @@ function fish_user_key_bindings
   # by hakshu
   bind \c] ghq_peco_repo
   bind \cr peco_select_history
+  bind \cg peco_git_checkout
 end
---
+++ .config/fish/functions/peco_git_checkout.fish
@@ -0,0 +1,16 @@
+# by hakshu
+
+function peco_git_checkout
+   git branch -a | peco | tr -d ' ' | read branch
+   echo $branch
+   if [ $branch ]
+       if contains $branch "remotes/"
+           set -l b (echo $branch | awk -F'/' '{print $3}')
+           git checkout -b $b $branch
+       else
+           git checkout $branch
+       end
+   end
+   commandline -f repaint
+end
+
